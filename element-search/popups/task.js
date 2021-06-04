624e03aa5427180d66d57ca6529dbf3115e64791
Vadim58 <awesome.rdeg@yandex.ru>
2021-06-04 13:42:30 +0300

    First task complete

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,7 +1,15 @@
 let modalMain = document.getElementById('modal_main');
-console.log(modalMain);
-modalMain.classList.add('modal_active');
+let modalSuccess = document.getElementById('modal_success');
+modalMain.classList.add('modal_active'); 
+let closeTimes = Array.from(document.getElementsByClassName('modal__close_times'));
+let closeMain = closeTimes[0];
+let closeSuccess = closeTimes[1];
+let dangerButton = document.querySelectorAll('a.show-success');
+let successButton = document.querySelectorAll('a.btn_success');
+closeMain.onclick = () => modalMain.classList.remove('modal_active');
+dangerButton[0].onclick = () =>  {
+	modalMain.classList.remove('modal_active'); 
+	modalSuccess.classList.add('modal_active');
+} 
+closeSuccess.onclick = () => modalSuccess.classList.remove('modal_active');
 
-let modalCloseCross = document.querySelectorAll('div.modal__close');
-console.log(modalCloseCross[0]);
-modalCloseCross[0].onclick = () => modalMain.classList.remove('modal_active');
