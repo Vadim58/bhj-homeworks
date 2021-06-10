263c749603bba6f7d0fc7cf500888108772d9960
Vadim58 <awesome.rdeg@yandex.ru>
2021-06-10 17:31:57 +0300

    New fix

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -6,13 +6,11 @@ arrowNext.onclick = () => {
     let index = picArray.findIndex(picture => picture.className.includes('slider__item_active'));
     picArray[index].classList.remove('slider__item_active');
     (index < picArray.length - 1) ? index += 1 : index = 0;
-	 picArray[index].classList.add('slider__item_active'); 
-     return false;
+	 picArray[index].classList.add('slider__item_active');    
 }
 arrowPrev.onclick = () => {
     let index = picArray.findIndex(picture => picture.className.includes('slider__item_active'));
     picArray[index].classList.remove('slider__item_active');
     (index > 0) ? index -= 1 : index = picArray.length - 1;
      picArray[index].classList.add('slider__item_active');
-    return false;
 }
