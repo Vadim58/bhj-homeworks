7ab6df499f712ca1ff92d50221d4bf4cd0760e36
Vadim58 <awesome.rdeg@yandex.ru>
2021-06-10 20:58:04 +0300

    end I hope

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,9 +1,11 @@
 let linksArray = document.querySelectorAll('ul.menu_main > li > a');
-let subMenu = Array.from(document.querySelectorAll('li ul'));
 for (let i = 0; i < linksArray.length; i++) {
     linksArray[i].onclick = () => {
-        index = subMenu.findIndex(menuItem => (menuItem.closest('li') === linksArray[i].closest('li')));
-        subMenu[index].classList.toggle('menu_active');
-        return false;
+		let highListItem = linksArray[i].closest('li');
+		let submenu = highListItem.querySelector('ul'); 
+		if (submenu) {
+		submenu.classList.toggle('menu_active'); 
+		return false
+		}        
     }
 }
