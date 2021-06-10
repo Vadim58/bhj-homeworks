b374ef7b65bc72998c3bfbadbfe3784c63db016f
Vadim58 <awesome.rdeg@yandex.ru>
2021-06-10 21:01:37 +0300

    the end

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,11 +1,11 @@
 let linksArray = document.querySelectorAll('ul.menu_main > li > a');
 for (let i = 0; i < linksArray.length; i++) {
     linksArray[i].onclick = () => {
-		let highListItem = linksArray[i].closest('li');
-		let submenu = highListItem.querySelector('ul'); 
-		if (submenu) {
-		submenu.classList.toggle('menu_active'); 
-		return false
-		}        
+        let highListItem = linksArray[i].closest('li');
+        let submenu = highListItem.querySelector('ul');
+        if (submenu) {
+            submenu.classList.toggle('menu_active');
+            return false
+        }
     }
 }
