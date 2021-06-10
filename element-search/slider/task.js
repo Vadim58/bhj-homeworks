629aa096d5cf568036aabd3b6dc0e024d8be5825
Vadim58 <awesome.rdeg@yandex.ru>
2021-06-10 16:15:19 +0300

    Slider finally fix

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -3,29 +3,18 @@ let arrowPrev = document.querySelector('div.slider__arrow_prev');
 let picArray = Array.from(document.querySelectorAll('.slider__item'));
 
 arrowNext.onclick = () => {
-  let index = picArray.findIndex(picture => picture.className.includes('slider__item_active'));
-  if(index < picArray.length - 1) {
-  picArray[index].classList.remove('slider__item_active');
-  picArray[index + 1].classList.add('slider__item_active');
-  return false;
-  }
-  else {
-	   picArray[index].classList.remove('slider__item_active');
-       picArray[0].classList.add('slider__item_active');
-	   return false;
-  }
+    let index = picArray.findIndex(picture => picture.className.includes('slider__item_active'));
+    picArray[index].classList.remove('slider__item_active');
+    (index < picArray.length - 1) ?
+    picArray[index + 1].classList.add('slider__item_active'):
+        picArray[0].classList.add('slider__item_active');
+    return false;
 }
-
 arrowPrev.onclick = () => {
-  let index = picArray.findIndex(picture => picture.className.includes('slider__item_active'));
-  if(index > 0){
-  picArray[index].classList.remove('slider__item_active');
-  picArray[index - 1].classList.add('slider__item_active');
-  return false;
-  }
-  else {
-	  picArray[index].classList.remove('slider__item_active');
-	  picArray[picArray.length - 1].classList.add('slider__item_active');
-	  return false;
-  }
+    let index = picArray.findIndex(picture => picture.className.includes('slider__item_active'));
+    picArray[index].classList.remove('slider__item_active');
+    (index > 0) ?
+    picArray[index - 1].classList.add('slider__item_active'):
+        picArray[picArray.length - 1].classList.add('slider__item_active');
+    return false;
 }
