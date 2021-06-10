2eab41316fe3436c8aae8308d5afe26374729821
Vadim58 <awesome.rdeg@yandex.ru>
2021-06-10 19:10:38 +0300

    Final fix

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,11 +1,9 @@
-let linksArray = Array.from(document.querySelectorAll('ul.menu_main a.menu__link'));
+let linksArray = document.querySelectorAll('ul.menu_main > li > a');
 let subMenu = Array.from(document.querySelectorAll('li ul'));
 for (let i = 0; i < linksArray.length; i++) {
-  linksArray[i].onclick = () => {
-	    listItem[ ].classList.toggle('menu_active');
-	    return false;
-  }
+    linksArray[i].onclick = () => {
+        index = subMenu.findIndex(menuItem => (menuItem.closest('li') === linksArray[i].closest('li')));
+        subMenu[index].classList.toggle('menu_active');
+        return false;
+    }
 }
-
-
-
