4bd30636a4b6bec835ff1f78abd731384a866b55
Vadim58 <awesome.rdeg@yandex>
2021-06-11 11:17:47 +0300

    next commit

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,25 +1,24 @@
-const arrow = document.querySelector('div.dropdown__value');
-console.log(arrow);
-const dropDown = document.querySelector('.dropdown__list');
-const listItem = document.querySelectorAll('li');
-console.log(listItem);
+let arrow = document.querySelector('div.dropdown__value');
+let dropDown = document.querySelector('.dropdown__list');
+let listItem = document.querySelectorAll('li');
 
 
 
 function cliik () {
-	dropDown.classList.add('dropdown__list_active');
+	dropDown.classList.toggle('dropdown__list_active');
 	return false
 	
 }
 
 function lil () {
+	
+	dropDown.classList.remove('dropdown__list_active');
 	arrow.textContent = "hooy";
-	return false
+	
 }
 
 arrow.addEventListener('click', cliik);
 
 for (let i = 0; i < listItem.length; i++){
 listItem[i].addEventListener('click', lil);
-
 }
