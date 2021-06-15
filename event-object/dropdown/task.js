83c9faaafbc4c22bc2eaa71746c8bd606428f61e
Vadim58 <awesome.rdeg@yandex.ru>
2021-06-15 15:46:27 +0300

    first commit

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,29 +1,42 @@
 let mainButton = document.querySelector('div.dropdown__value');
 let dropDown = document.querySelector('.dropdown__list');
 let listItem = Array.from(document.querySelectorAll('li'));
+let dropDownLink = Array.from(document.querySelectorAll('a'));
 
 
-function mainClick () {
+mainClick = () => {	
 	dropDown.classList.toggle('dropdown__list_active');
 	return false	
 }
 
-function itemClick () {
+itemClick = () => {
+	for (i = 0; i < dropDownLink.length; i++) {
+	dropDown.classList.remove('dropdown__list_active');
+	mainButton.textContent = dropDownLink[i].textContent;
+	console.log(dropDownLink[i].textContent); 	
+	}
 	
-	//dropDown.classList.remove('dropdown__list_active');
-	mainButton.textContent = 'ffff';
-    return false
 }
 
 
 //ОБРАБОТЧИКИ СОБЫТИЙ
 
-mainButton.addEventListener('click', mainClick);
+
+mainButton.addEventListener('click', function () {
+	dropDown.classList.toggle('dropdown__list_active');
+	return false	
+});
+
+for (let i = 0; i < dropDownLink.length; i++) {
+dropDownLink[i].addEventListener('click', function () {
+	for (i = 0; i < dropDownLink.length; i++) {
+	dropDown.classList.remove('dropdown__list_active');
+	mainButton.textContent = dropDownLink[i].textContent;
+	console.log(dropDownLink[i].textContent); 	
+	}
+});
+}
 
 
-	
-listItem.addEventListener('click', itemClick);
 
-
-
-
+ 
--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,15 +17,14 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
-  }
-
+    window.addEventListener('keyup', event => {
+        if (event.key.toUpperCase() === this.currentSymbol.textContent.toUpperCase()) {
+            return this.success()
+        } else {
+            return this.fail()
+        }
+    });
+}
   success() {
     this.currentSymbol.classList.add('symbol_correct');
     this.currentSymbol = this.currentSymbol.nextElementSibling;
