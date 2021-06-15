60ea9c5e43e1ae30e13b252a8101ee680b390a6b
Vadim58 <awesome.rdeg@yandex.ru>
2021-06-15 18:26:49 +0300

    little fix

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,26 +1,6 @@
 let mainButton = document.querySelector('div.dropdown__value');
 let dropDown = document.querySelector('.dropdown__list');
-let listItem = Array.from(document.querySelectorAll('li'));
 let dropDownLink = Array.from(document.querySelectorAll('a'));
-
-
-mainClick = () => {	
-	dropDown.classList.toggle('dropdown__list_active');
-	return false	
-}
-
-itemClick = () => {
-	for (i = 0; i < dropDownLink.length; i++) {
-	dropDown.classList.remove('dropdown__list_active');
-	mainButton.textContent = dropDownLink[i].textContent;
-	console.log(dropDownLink[i].textContent); 	
-	}
-	
-}
-
-
-//ОБРАБОТЧИКИ СОБЫТИЙ
-
 
 mainButton.addEventListener('click', function () {
 	dropDown.classList.toggle('dropdown__list_active');
@@ -32,7 +12,6 @@ dropDownLink[i].addEventListener('click', function () {
 	for (i = 0; i < dropDownLink.length; i++) {
 	dropDown.classList.remove('dropdown__list_active');
 	mainButton.textContent = dropDownLink[i].textContent;
-	console.log(dropDownLink[i].textContent); 	
 	}
 });
 }
