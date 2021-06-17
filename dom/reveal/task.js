2e70ea0ba03b62f8508e13eaab5d0dc9e35cfbf5
Vadim58 <awesome.rdeg@yandex.ru>
2021-06-17 16:31:16 +0300

    First commit

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -2,12 +2,16 @@ const block = document.querySelector('.reveal');
 console.log(block);
 let inform = block.getBoundingClientRect();
 const viewPort = window.innerHeight;
-console.log(inform);
-console.log(viewPort - inform.top);
+console.log(`верх ${inform.top}`);
+console.log(`низ ${inform.bottom}`);
+console.log(`окно ${viewPort}`);
+console.log( - inform.height);
 window.addEventListener('scroll', function() {
-	if (viewPort > inform.top) {
-		
+	if ((viewPort > inform.top) && (inform.bottom > 0 && inform.top > - inform.height) ) {	
 	console.log('ssss');
+	block.classList.add('reveal_active');
+	
 	}
-	}
+	return false;
+  }
 )
