6daf4db3b6c9de11c84cc19bb9a34fc79d635010
Vadim58 <awesome.rdeg@yandex.ru>
2021-06-21 16:17:58 +0300

    New fix

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,10 +1,9 @@
 const rotator = Array.from(document.querySelectorAll('.rotator__case'));
 let index = rotator.findIndex(span => span.classList.contains('rotator__case_active'));
-	function rotatorChange(){			
-			rotator[index].classList.remove('rotator__case_active');
-			(index < rotator.length - 1) ? index += 1 : index = 0			
-	        rotator[index].classList.add('rotator__case_active');		
-	}	
+
+function rotatorChange() {
+    rotator[index].classList.remove('rotator__case_active');
+    (index < rotator.length - 1) ? index += 1: index = 0
+    rotator[index].classList.add('rotator__case_active');
+}
 setInterval(rotatorChange, 1000);
-
-
--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,14 +1,12 @@
-const block =Array.from(document.querySelectorAll('.reveal'));
+const block = Array.from(document.querySelectorAll('.reveal'));
 const viewPort = window.innerHeight;
-for (let i = 0; i < block.length; i++){
-	let blockLocation = block[i].getBoundingClientRect();
+for (let i = 0; i < block.length; i++) {
+    let blockLocation = block[i].getBoundingClientRect();
     window.addEventListener('scroll', function() {
-	if ((viewPort > blockLocation.top) && (blockLocation.bottom > 0 && blockLocation.top > - blockLocation.height) ) {	
-	block[i].classList.add('reveal_active');
-	}
-	else {
-		block[i].classList.remove('reveal_active');
-   }
-  }
- )
+        if ((viewPort > blockLocation.top) && (blockLocation.bottom > 0 && blockLocation.top > -blockLocation.height)) {
+            block[i].classList.add('reveal_active');
+        } else {
+            block[i].classList.remove('reveal_active');
+        }
+    })
 }
