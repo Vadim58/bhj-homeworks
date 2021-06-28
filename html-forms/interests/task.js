9f8776052ed8ab3b50ed4b3d6687a9a1cc3232ef
Vadim58 <awesome.rdeg@yandex.ru>
2021-06-28 22:09:42 +0300

    Second fix

---
+++ html-forms/interests/task.js
@@ -0,0 +1,30 @@
+let highLevelCheckBox = Array.from(document.querySelectorAll('div > ul > .interest > label > .interest__check'));
+let highList = document.querySelectorAll('div > ul > .interest');
+let listOfFood = highList[0].querySelector('ul');
+let foodListItem = Array.from(listOfFood.querySelectorAll('li label .interest__check'));
+let listOfPets = highList[1].querySelector('ul');
+let petsListItem = Array.from(listOfPets.querySelectorAll('li label .interest__check'));
+
+
+
+highLevelCheckBox[0].addEventListener('change', function() {
+    for (let k = 0; k < foodListItem.length; k++) {
+        if (foodListItem[k].checked === false) {
+            foodListItem[k].checked = true;
+        } else {
+            foodListItem[k].checked = false;
+        }
+    }
+
+})
+
+highLevelCheckBox[1].addEventListener('change', function() {
+    for (let i = 0; i < petsListItem.length; i++) {
+        if (petsListItem[i].checked === false) {
+            petsListItem[i].checked = true;
+        } else {
+            petsListItem[i].checked = false;
+        }
+    }
+
+})
