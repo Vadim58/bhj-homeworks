f8edd546717de456593a7430c6946512ff50f89c
Vadim58 <awesome.rdeg@yandex>
2021-07-01 09:42:36 +0300

    origin master

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -2,7 +2,34 @@ const tasksList = document.getElementById('tasks__list');
 console.log(tasksList);
 const taskInput = document.getElementById('task__input');
 console.log(taskInput);
+const tasksAdd = document.getElementById('tasks__add');
+const taskRemove = document.querySelector('.task__remove');
 taskInput.addEventListener('keyup', function(e) {
-
-		console.log(taskInput.value);
+	  e.preventDefault();
+		if(e.key === 'Enter'){
+	  tasksList.innerHTML += `
+       <div class="task">
+  <div class="task__title">
+    ${taskInput.value}
+  </div>
+  <a href="#" class="task__remove">&times;</a>
+</div>
+	  `
+	  taskInput.value = null;
+	}
 })
+tasksAdd.addEventListener('click', function(e) {
+	  e.preventDefault();
+	  tasksList.innerHTML += `
+       <div class="task">
+  <div class="task__title">
+    ${taskInput.value}
+  </div>
+  <a href="#" class="task__remove">&times;</a>
+</div>
+	  `
+	  taskInput.value = null;	
+})
+
+taskRemove.onclick = () => 
+
