021b27e41a688fa592fbaed93998a441b608ec73
Vadim58 <awesome.rdeg@yandex.ru>
2021-07-01 16:53:51 +0300

    Second task final fix

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,44 +1,44 @@
 const tasksList = document.getElementById('tasks__list');
-console.log(tasksList);
 const taskInput = document.getElementById('task__input');
-console.log(taskInput);
 const tasksAdd = document.getElementById('tasks__add');
 
 taskInput.addEventListener('keyup', function(e) {
-	  e.preventDefault();
-		if(e.key === 'Enter' && taskInput.value.length != 0){
-	  tasksList.innerHTML += `
+    e.preventDefault();
+    if (e.key === 'Enter' && taskInput.value.trim().length > 0) {
+        tasksList.innerHTML += `
        <div class="task">
   <div class="task__title">
     ${taskInput.value}
   </div>
   <a href="#" class="task__remove">&times;</a>
 </div>
-	  `
-	  taskInput.value = null;
-	  let taskRemove = document.querySelectorAll('.task__remove');
-for (let i = 0; i < taskRemove.length; i++){
-taskRemove[i].addEventListener("click", function(){
-        taskRemove[i].parentNode.parentNode.removeChild(taskRemove[i].parentNode);
-    });
-  }
-	}
+    `
+        taskInput.value = null;
+        let taskRemove = document.querySelectorAll('.task__remove');
+        for (let i = 0; i < taskRemove.length; i++) {
+            taskRemove[i].addEventListener("click", function() {
+                taskRemove[i].parentNode.parentNode.removeChild(taskRemove[i].parentNode);
+            });
+        }
+    }
 })
 tasksAdd.addEventListener('click', function(e) {
-	  e.preventDefault();
-	  tasksList.innerHTML += `
+    e.preventDefault();
+    if (taskInput.value.trim().length > 0) {
+        tasksList.innerHTML += `
        <div class="task">
   <div class="task__title">
     ${taskInput.value}
   </div>
   <a href="#" class="task__remove">&times;</a>
 </div>
-	  `
-	  taskInput.value = null;	
-let taskRemove = document.querySelectorAll('.task__remove');
-for (let i = 0; i < taskRemove.length; i++){
-taskRemove[i].addEventListener("click", function(){
-        taskRemove[i].parentNode.parentNode.removeChild(taskRemove[i].parentNode);
-    });
-  }
+    `
+        taskInput.value = null;
+        let taskRemove = document.querySelectorAll('.task__remove');
+        for (let i = 0; i < taskRemove.length; i++) {
+            taskRemove[i].addEventListener("click", function() {
+                taskRemove[i].parentNode.parentNode.removeChild(taskRemove[i].parentNode);
+            });
+        }
+    }
 })
