dbdaeed1f6626802de360003e657617369873438
Vadim58 <awesome.rdeg@yandex.ru>
2021-07-02 18:06:51 +0300

    3rd task final fix

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -6,46 +6,34 @@ const numberOfGoods = document.querySelectorAll('.product__quantity-value');
 const cart = document.querySelector('.cart__products');
 const addButton = document.querySelectorAll('.product__add');
 for (let i = 0; i < product.length; i++) {
-    incButton[i].addEventListener('click', function() {
-        numberOfGoods[i].textContent = Number(numberOfGoods[i].textContent) + 1;
+  incButton[i].addEventListener('click', function() {
+    numberOfGoods[i].textContent = Number(numberOfGoods[i].textContent) + 1;
+  })
 
-    })
+  decButton[i].addEventListener('click', function() {
+    if (Number(numberOfGoods[i].textContent) > 1) {
+      numberOfGoods[i].textContent = Number(numberOfGoods[i].textContent) - 1;
+    }
+  })
 
-    decButton[i].addEventListener('click', function() {
-        if (Number(numberOfGoods[i].textContent) > 1) {
-            numberOfGoods[i].textContent = Number(numberOfGoods[i].textContent) - 1;
-        }
-    })
-
-       
-    addButton[i].addEventListener('click', function() {
-         let cartProduct = Array.from(document.querySelectorAll('.cart__product'));
-         let cartProductsNumber = document.querySelectorAll('.cart__product_count');
-         let findedCart = cartProduct.find(item => item.getAttribute('data-id') === product[i].getAttribute('data-id'));
-         
-         if(cartProduct.find(item => item.getAttribute('data-id') === product[i].getAttribute('data-id'))){
-         cartProductsNumber[i].textContent = Number(cartProductsNumber[i].textContent) + Number(numberOfGoods[i]);
-     }   
-     
-    else {
-        cart.innerHTML += `
+  addButton[i].addEventListener('click', function() {
+    const cartProducts = Array.from(document.querySelectorAll('.cart__product'));
+    const productInCart = cartProducts.find(item => item.getAttribute('data-id') === product[i].getAttribute('data-id'));
+    if (productInCart) {
+      let cartProductsValue = productInCart.querySelector('.cart__product-count');
+      cartProductsValue.textContent = Number(cartProductsValue.textContent) + Number(numberOfGoods[i].textContent);
+    } else {
+      cart.innerHTML += `
       <div class="cart__product" data-id="${product[i].getAttribute('data-id')}">
     <img class="cart__product-image" src="${image[i].getAttribute('src')}">
     <div class="cart__product-count">${numberOfGoods[i].textContent}</div>
     </div>
       `
     }
-    })
-
-       
-
-       
-
+  })
 }
 
 
-          
-      // cartProductsNumber[i].textContent = Number(cartProductsNumber[i].textContent) + Number(numberOfGoods[i]);
 
 
 
