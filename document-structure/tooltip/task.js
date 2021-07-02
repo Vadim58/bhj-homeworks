f8662c0e2e0a95250b00643d720b242a0e38f035
Vadim58 <awesome.rdeg@yandex.ru>
2021-07-02 18:47:44 +0300

    Final

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,11 +1,16 @@
 const hasTooltip = document.querySelectorAll('.has-tooltip');
 for (let i = 0; i < hasTooltip.length; i++) {
-
     let position = hasTooltip[i].getBoundingClientRect();
     hasTooltip[i].addEventListener('click', function(event) {
-        event.preventDefault();
-        hasTooltip[i].outerHTML += `
+       event.preventDefault();
+       let tooltips = Array.from(document.querySelectorAll('.tooltip'));
+       let item = tooltips.find(item => item.classList.contains('tooltip_active'))
+       if(item){
+         item.classList.remove('tooltip_active');
+       }
+       else {
+        hasTooltip[i].insertAdjacentHTML('afterbegin', `
       <div class="tooltip tooltip_active" style = "left: ${position.left}px; top: ${position.top + 15}px; position: absolute;">${this.title}
-      </div>`
+      </div>`)}
     });
 }
