11e5aba11df88e9de51fc561ab66cb3b1895f41f
Vadim58 <awesome.rdeg@yandex.ru>
2021-07-02 10:54:10 +0300

    Second task fix

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -2,43 +2,39 @@ const tasksList = document.getElementById('tasks__list');
 const taskInput = document.getElementById('task__input');
 const tasksAdd = document.getElementById('tasks__add');
 
+function addNewTask() {
+  tasksList.insertAdjacentHTML( 'afterbegin', `
+     <div class="task">
+    <div class="task__title">
+        ${taskInput.value}
+    </div>
+    <a href="#" class="task__remove">&times;</a>
+</div>
+    `);
+  taskInput.value = null;
+}
+
+function removeTask(){
+  const taskRemove = document.querySelector('.task__remove'); 
+      taskRemove.addEventListener("click", function() {
+        taskRemove.parentNode.parentNode.removeChild(taskRemove.parentNode);
+      });
+}
+
 taskInput.addEventListener('keyup', function(e) {
-    e.preventDefault();
-    if (e.key === 'Enter' && taskInput.value.trim().length > 0) {
-        tasksList.innerHTML += `
-       <div class="task">
-  <div class="task__title">
-    ${taskInput.value}
-  </div>
-  <a href="#" class="task__remove">&times;</a>
-</div>
-    `
-        taskInput.value = null;
-        let taskRemove = document.querySelectorAll('.task__remove');
-        for (let i = 0; i < taskRemove.length; i++) {
-            taskRemove[i].addEventListener("click", function() {
-                taskRemove[i].parentNode.parentNode.removeChild(taskRemove[i].parentNode);
-            });
-        }
-    }
+  e.preventDefault();
+  if (e.key === 'Enter' && taskInput.value.trim().length > 0) {
+    addNewTask();
+    removeTask();
+  }
 })
+
 tasksAdd.addEventListener('click', function(e) {
-    e.preventDefault();
-    if (taskInput.value.trim().length > 0) {
-        tasksList.innerHTML += `
-       <div class="task">
-  <div class="task__title">
-    ${taskInput.value}
-  </div>
-  <a href="#" class="task__remove">&times;</a>
-</div>
-    `
-        taskInput.value = null;
-        let taskRemove = document.querySelectorAll('.task__remove');
-        for (let i = 0; i < taskRemove.length; i++) {
-            taskRemove[i].addEventListener("click", function() {
-                taskRemove[i].parentNode.parentNode.removeChild(taskRemove[i].parentNode);
-            });
-        }
-    }
+  e.preventDefault();
+  if (taskInput.value.trim().length > 0) {
+    addNewTask();
+    removeTask();
+  }
 })
+
+
--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,10 +1,13 @@
 const hasTooltip = document.querySelectorAll('.has-tooltip');
 for (let i = 0; i < hasTooltip.length; i++) {
+
     let position = hasTooltip[i].getBoundingClientRect();
     hasTooltip[i].addEventListener('click', function(event) {
         event.preventDefault();
         hasTooltip[i].outerHTML += `
-      <div class="tooltip tooltip_active" style = "left: ${position.left}px; top: ${position.top + 15}px; ">${this.title}
+      <div class="tooltip tooltip_active" style = "left: ${position.left}px; top: ${position.top + 15}px; position: absolute;">${this.title}
       </div>`
+       console.log(hasTooltip[i].getAttribute('href'));
+      
     });
 }
