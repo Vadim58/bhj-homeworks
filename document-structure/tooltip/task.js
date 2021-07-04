a241083468fd84fb8a279949c3a4a62528cd46cf
Vadim58 <awesome.rdeg@yandex.ru>
2021-07-04 19:39:23 +0300

    final fix

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,16 +1,14 @@
 const hasTooltip = document.querySelectorAll('.has-tooltip');
 for (let i = 0; i < hasTooltip.length; i++) {
-    let position = hasTooltip[i].getBoundingClientRect();
-    hasTooltip[i].addEventListener('click', function(event) {
-       event.preventDefault();
-       let tooltips = Array.from(document.querySelectorAll('.tooltip'));
-       let item = tooltips.find(item => item.classList.contains('tooltip_active'))
-       if(item){
-         item.classList.remove('tooltip_active');
-       }
-       else {
-        hasTooltip[i].insertAdjacentHTML('afterbegin', `
+  let position = hasTooltip[i].getBoundingClientRect();
+  hasTooltip[i].addEventListener('click', function(event) {
+    event.preventDefault();
+    if (this.querySelector('div')) {
+      this.querySelector('div').remove();
+    } else {
+      this.insertAdjacentHTML('afterbegin', `
       <div class="tooltip tooltip_active" style = "left: ${position.left}px; top: ${position.top + 15}px; position: absolute;">${this.title}
-      </div>`)}
-    });
+      </div>`);
+    }
+  });
 }
