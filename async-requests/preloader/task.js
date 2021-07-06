cb751660821b4a87395c1d79545b438463846e22
Vadim58 <awesome.rdeg@yandex.ru>
2021-07-06 20:01:32 +0300

    First task

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -2,28 +2,25 @@ let loader = document.getElementById('loader');
 let xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://netology-slow-rest.herokuapp.com');
 xhr.send();
-xhr.addEventListener('readystatechange', function () {
-	if (xhr.readyState === xhr.DONE) {
-		loader.classList.remove('loader_active');
-		let currencyRates = JSON.parse(this.responseText);
-		console.log(currencyRates.response.Valute);
-		let item = document.getElementById('items');
-		console.log(item);
-		item.innerHTML +=`
+xhr.addEventListener('readystatechange', function() {
+  if (xhr.readyState === xhr.DONE) {
+    loader.classList.remove('loader_active');
+    let currencyRates = JSON.parse(this.responseText);
+    for (current in currencyRates.response.Valute) {
+      let item = document.getElementById('items');
+      item.insertAdjacentHTML('afterbegin', `
+		 <div class = 'item'>
 		 <div class="item__code">
-    ${currencyRates.response.Valute.AMD.CharCode}
+    ${currencyRates.response.Valute[current].CharCode}
 </div>
 <div class="item__value">
-    ${currencyRates.response.Valute.AMD.Value}
+    ${currencyRates.response.Valute[current].Value}
 </div>
 <div class="item__currency">
  руб.
  </div>
-		
-		`
-		
-		
-		
-		
-	}
+ </div>
+		`)
+    }
+  }
 })
