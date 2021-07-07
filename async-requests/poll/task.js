8ab47a01549261afa72709942e48bcea79c53444
Vadim58 <awesome.rdeg@yandex>
2021-07-07 16:22:16 +0300

    second task complete

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -3,22 +3,23 @@ xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/poll.php');
 xhr.send();
 xhr.addEventListener('readystatechange', function() {
 
-if (xhr.readyState === xhr.DONE) {
-let response = JSON.parse(this.responseText);
-console.log(response);
-let pollTitle = document.getElementById('poll__title');
-let pollAnswers = document.getElementById('poll__answers');
-console.log(response.data.answers);
-let answers = response.data.answers;
-pollTitle.innerText = `${response.data.title}`;
-for (let i = 0; i < answers.length; i++) {
-pollAnswers.innerHTML += `
+  if (xhr.readyState === xhr.DONE) {
+    let response = JSON.parse(this.responseText);
+    let pollTitle = document.getElementById('poll__title');
+    let pollAnswers = document.getElementById('poll__answers');
+    let answers = response.data.answers;
+    pollTitle.innerText = `${response.data.title}`;
+    for (let i = 0; i < answers.length; i++) {
+      pollAnswers.innerHTML += `
    <button class="poll__answer">
           ${answers[i]}
         </button>
 `
-}
-let button = document.querySelectorAll('');
-
-}
+    }
+    let button = Array.from(document.querySelectorAll('.poll__answer'));
+    button.forEach(item =>
+      item.addEventListener('click', function() {
+        alert('Спасибо, Ваш голос засчитан!');
+      }));
+  }
 })
