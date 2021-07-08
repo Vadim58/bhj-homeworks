0dd1ccce341f785266b801859e6c319120545ac3
Vadim58 <awesome.rdeg@yandex>
2021-07-08 11:42:07 +0300

    fix

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -2,3 +2,6 @@ let progressBar = document.getElementById('progress');
 console.log(progressBar.value);
 var formData = new FormData();
 
+let xhr = new XMLHttpRequest();
+xhr.open("POST", "https://netology-slow-rest.herokuapp.com/upload.php");
+xhr.send(formData);
