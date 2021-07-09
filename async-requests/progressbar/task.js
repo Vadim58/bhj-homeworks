c6d589b65d572f385a84e34577681306e83c1a0e
Vadim58 <awesome.rdeg@yandex.ru>
2021-07-09 09:34:09 +0300

    final fix

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,32 +1,18 @@
-let progressBar = document.getElementById('progress');
-function barEnlarge() {
-  if (progressBar.value < 0.8) {
-    progressBar.value = progressBar.value + 0.001;
-  }
-}
-let form = document.getElementById('form');
+const progressBar = document.getElementById('progress');
+const form = document.getElementById('form');
 form.addEventListener('submit', function(e) {
   e.preventDefault();
-  let formData = new FormData(form);
-  let xhr = new XMLHttpRequest();
+  const formData = new FormData(form);
+  const xhr = new XMLHttpRequest();
+  xhr.upload.addEventListener('progress', function(e) {
+    progressBar.value = e.loaded / e.total;
+  });
+  xhr.upload.addEventListener('error', function() {
+    alert('произошла ошибка!');
+  });
+  xhr.upload.addEventListener('load', function() {
+    alert('данные загружены');
+  });
   xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
   xhr.send(formData);
-  xhr.addEventListener('readystatechange', function() {
-    if (xhr.readyState === xhr.UNSENT) {
-      progressBar.value = 0.02;
-    }
-    if (xhr.readyState === xhr.OPENED) {
-      progressBar.value = 0.04;
-    }
-    if (xhr.readyState === xhr.HEADERS_RECEIVED) {
-      progressBar.value = 0.1;
-      setInterval(barEnlarge, 100);
-    }
-    if (xhr.readyState === xhr.LOADING) {
-      setInterval(barEnlarge, 30000);
-    }
-    if (xhr.readyState === xhr.DONE) {
-      progressBar.value = 1;
-    }
-  })
-})
+});
